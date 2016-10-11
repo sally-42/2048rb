ef3609491ed6f7eb6f9a20b3475c6d9e90d636fd
mbigras <mbigras22@gmail.com>
2016-10-11 02:48:55 -0700

    Clean p and puts

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'ap'
-puts "Always slam left to right"
+
 a = [
   ['00', '01', '02', '03'],
   ['10', '11', '12', '13'],
@@ -7,21 +7,21 @@ a = [
   ['30', '31', '32', '33'],
 ]
 
-puts "left is left, right is right"
+
 a_lr = a
 ap a_lr
 
-puts "left is right, right is left"
+
 a_rl = a.map do |row|
   row.reverse
 end
 ap a_rl
 
-puts "left is up, right is down"
+
 a_ud = a.transpose
 ap a_ud
 
-puts "left is down, right is up"
+
 a_du = a.transpose.map do |col|
   col.reverse
 end
--- process.rb
+++ process.rb
@@ -1,8 +1,8 @@
 a = [nil, 2, 2, 2]
 def process(array, i, j, counter)
   if counter > array.length
-    p "hit base"
-    p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
+
     return array
   end
 
@@ -13,46 +13,46 @@ def process(array, i, j, counter)
     process(array, i, j, counter)
   end
 
-  puts "="*20
-  puts "*"*20
-  puts "="*20
-  puts "$"*20
-  p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
+
+
+
+
   if array[i].nil?
     if array[j].nil?
-      puts "do nothing, increment j"
-      p "nothing: #{array[i]} == nil; #{array[j]} == nil"
+
+
       counter += 1
       process(array, i, j, counter)
     else
       # swap
-      p "swap! repeat search from i: #{i}, j: #{i+1}"
-      p "swap: #{array[i]} == nil; #{array[j]} != nil"
-      p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
+
+
       array[i] = array[j]
       array[j] = nil
-      p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
       counter = 0
       process(array, i, i+1, counter)
     end
   else
-    p "#{array[i]} != nil"
-    p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
+
     if array[j].nil?
       # incremement j
-      p "do nothing, inc j"
-      p "nothing: #{array[i]} != nil; #{array[j]} == nil"
+
+
       counter += 1
       process(array, i, j+1, counter)
     else
-      p "i:#{i};j#{j};array[i]:#{array[i]};j#{j};array:#{array}; counter:#{counter}"
+
       if array[j] == array[i]
         array[i] = 2*array[i]
         array[j] = nil
         counter = 0
         process(array, i+1, j+1, counter)
       else
-        puts "the current cell is blocked increment i"
+
         i += 1
         counter = 0
         process(array, i, i+1,counter)
@@ -60,7 +60,6 @@ def process(array, i, j, counter)
     end
   end
 end
-
 p a
-ap = process(a,0,1,0)
+process(a,0,1,0)
 p a
