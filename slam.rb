2a20508082715b24ade92f9bf3a9099749e051a2
mbigras <mbigras22@gmail.com>
2016-10-11 03:01:23 -0700

    Rename process -> slam

--- slam.rb
+++ slam.rb
@@ -1,5 +1,4 @@
-a = [nil, 2, 2, 2]
-def process(array, i, j, counter)
+def slam(array, i, j, counter)
   if counter > array.length
     return array
   end
@@ -7,38 +6,35 @@ def process(array, i, j, counter)
     i += 1
     j = i
     counter = 0
-    process(array, i, j, counter)
+    slam(array, i, j, counter)
   end
   if array[i].nil?
     if array[j].nil?
       counter += 1
-      process(array, i, j, counter)
+      slam(array, i, j, counter)
     else
       # swap
       array[i] = array[j]
       array[j] = nil
       counter = 0
-      process(array, i, i+1, counter)
+      slam(array, i, i+1, counter)
     end
   else
     if array[j].nil?
       # incremement j
       counter += 1
-      process(array, i, j+1, counter)
+      slam(array, i, j+1, counter)
     else
       if array[j] == array[i]
         array[i] = 2*array[i]
         array[j] = nil
         counter = 0
-        process(array, i+1, j+1, counter)
+        slam(array, i+1, j+1, counter)
       else
         i += 1
         counter = 0
-        process(array, i, i+1,counter)
+        slam(array, i, i+1,counter)
       end
     end
   end
 end
-p a
-process(a,0,1,0)
-p a
