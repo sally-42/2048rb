fb62faf0d3b2a2369dceb8e765ec54b4e25468b8
mbigras <mbigras22@gmail.com>
2016-10-11 02:51:32 -0700

    Clean up blank lines

--- process.rb
+++ process.rb
@@ -1,58 +1,37 @@
 a = [nil, 2, 2, 2]
 def process(array, i, j, counter)
   if counter > array.length
-
-
     return array
   end
-
   if j == array.length
     i += 1
     j = i
     counter = 0
     process(array, i, j, counter)
   end
-
-
-
-
-
-
   if array[i].nil?
     if array[j].nil?
-
-
       counter += 1
       process(array, i, j, counter)
     else
       # swap
-
-
-
       array[i] = array[j]
       array[j] = nil
-
       counter = 0
       process(array, i, i+1, counter)
     end
   else
-
-
     if array[j].nil?
       # incremement j
-
-
       counter += 1
       process(array, i, j+1, counter)
     else
-
       if array[j] == array[i]
         array[i] = 2*array[i]
         array[j] = nil
         counter = 0
         process(array, i+1, j+1, counter)
       else
-
         i += 1
         counter = 0
         process(array, i, i+1,counter)
