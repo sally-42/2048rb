2f3320647f48fa52ddac2e4828f818723f75d6f5
mbigras <mbigras22@gmail.com>
2016-10-14 00:40:07 -0700

    Add copy and display example

--- app.rb
+++ app.rb
@@ -8,3 +8,15 @@ a = [
   ['', '', '', ''],
   ['', '', '4', ''],
 ]
+a_copy = a.map { |e| copy e }
+
+display a
+display a_copy
+
+p "="*26
+
+a[0][0] = "cats"
+a_copy[0][0] = "dogs"
+
+display a
+display a_copy
