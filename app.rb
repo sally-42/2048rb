3dd13b446e7e901402c4f35a1964fdf4076cf262
mbigras <mbigras22@gmail.com>
2016-10-14 00:35:06 -0700

    Add a practice array to app.rb

---
+++ app.rb
@@ -0,0 +1,10 @@
+require_relative "presentation_helpers.rb"
+require_relative "rotate_helpers.rb"
+require_relative "hit_helpers.rb"
+
+a = [
+  ['2', '', '4', '4'],
+  ['2', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '4', ''],
+]
